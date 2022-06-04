1e9f2608297a5cfdf7da63308376e992d6adf6d0
Jamie <jbuckley760@gmail.com>
2022-06-04 04:36:42 +0100

    Add initial flow for creation of recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,21 @@
 class RecipesController < ApplicationController
 
   def index
-    @user_recipes = Recipe.find_by_user_id(current_user.id)
+    @user_recipes = Recipe.where(user_id: current_user.id)
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    permitted  = params.require(:recipe).permit(:name)
+    @recipe = Recipe.new(permitted)
+    @recipe.user_id = current_user.id
+    if @recipe.save
+      redirect_to @recipe
+    else
+      render :new, status: 422
+    end
   end
 end
--- db/migrate/20220604002132_create_recipes.rb
+++ db/migrate/20220604002132_create_recipes.rb
@@ -1,7 +1,7 @@
 class CreateRecipes < ActiveRecord::Migration[7.0]
   def change
     create_table :recipes do |t|
-      t.string :name
+      t.string :name, null: false
       t.timestamps
       t.references :user, foreign_key: true, null: false
     end
