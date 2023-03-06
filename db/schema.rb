a351ef94f52a79316ce70fd6b7551edac2f9b8be
Jamie <jbuckley760@gmail.com>
2023-03-06 17:50:21 +0000

    Fixes to allow using linode object storage in place of s3

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 
 ActiveRecord::Schema[7.0].define(version: 2022_07_07_221702) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
