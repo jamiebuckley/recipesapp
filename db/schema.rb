f76c0265ec989b769988b59be1104802bc7daf5c
Jamie <jbuckley760@gmail.com>
2023-05-28 17:55:27 +0100

    Small fix to not show tag editor when recipe is not owned

--- db/schema.rb
+++ db/schema.rb
@@ -47,6 +47,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_28_154332) do
     t.string "category"
   end
 
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text "content"
+    t.string "searchable_type"
+    t.bigint "searchable_id"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable"
+  end
+
   create_table "recipe_ingredients", force: :cascade do |t|
     t.float "quantity", null: false
     t.string "unit", null: false
